0ae33f7ec951c0a3e593b42c062b8faa2db1a03c
Paulo-hs95 <paulo.henrisoares.ph@gmail.com>
2025-12-15 16:07:18 -0300

    Update Dash2.py

--- Dash2.py
+++ Dash2.py
@@ -225,7 +225,7 @@ for v1 in rt_tp:
 				        	hover_text.append(row)
 						
 				        exec(f'fig{month+1} = ff.create_annotated_heatmap(z, x=x, y=y1, annotation_text=z_text28, colorscale=colorscale, text=hover_text, hoverinfo="text")')
-				        exec(f'fig{month+1}.update_layout(title_text = f"Produtividade mês de {m[month]}", xaxis=dict(range=[0, 10]), yaxis=dict(range=[0, 10]))')
+				        exec(f'fig{month+1}.update_layout(title_text = f"Produtividade mês de {m[month]}")')
 
 
 
@@ -395,6 +395,7 @@ col9.plotly_chart(fig9)
 col10.plotly_chart(fig10)
 col11.plotly_chart(fig11)
 col12.plotly_chart(fig12)
+
 
 
 
